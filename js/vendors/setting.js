ca7831b62a2f081be1b5a42f690bf7671e12f04e
thanhdd <duythanh14189@gmail.com>
2024-06-21 11:56:55 +0700

    up

--- js/vendors/setting.js
+++ js/vendors/setting.js
@@ -7,24 +7,6 @@ $('.js-mobile').on('click', function(){
     $(".header-nav").fadeToggle();
 });
 
-$(function () {
-    var headerHeight = $('#header').outerHeight();
-    var urlHash = location.hash;
-    if (urlHash) {
-        $('body,html').stop().scrollTop(0);
-        setTimeout(function () {
-            var target = $(urlHash);
-            var position = target.offset().top - headerHeight;
-            $('body,html').stop().animate({scrollTop: position}, 1000);
-        }, 100);
-    }
-    
-    jQuery('.js-scroll').click(function(e) {
-        jQuery('html,body').animate({ scrollTop: jQuery(this.hash).offset().top - headerHeight}, 1500);
-        return false;
-        e.preventDefault();
-    });
-});
 
 // $(document).on("click", function(){
 // 	$("aa").hide();
